924927b1db681643a8bc60b050db80ca9e67bb4e
Gregg Christofferson <gechristofferson@gmail.com>
2018-09-17 15:01:56 -0700

    added getCurrentMonth and createForm functions

--- code.js
+++ code.js
@@ -1,3 +1,15 @@
+// create form, name and open it
+
+// collect email addresses (required)
+
+// get names of approved publishers from "Approved For Public Witnessing" group in Google Contacts
+
+// create the "Name" dropdown choice
+
+// add required question: "Did your availability change from last month?"
+
+// if "Yes" continue to next section, otherwise submit form
+
 function myFunction() {
   
 }
